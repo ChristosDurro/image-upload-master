cdd8c78a92113a0aaaf7e9730dd7f2d731493267
Christos Ntrourro <xristosdurros@hotmail.com>
2022-12-27 13:24:33 +0200

    updated responsivness and readme file

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -1,15 +1,13 @@
-
-import './Loading.css';
+import "./Loading.css";
 import { LinearProgress } from "@mui/material";
 
 const Loading = () => {
-    return (
-        <div className="container">
-            <h2 className="uploading-header">Uploading...</h2>
-            <LinearProgress className="progress-bar" />
-        </div>
-    )
-}
-
+  return (
+    <div className="uploading-container">
+      <h2 className="uploading-header">Uploading...</h2>
+      <LinearProgress className="progress-bar" />
+    </div>
+  );
+};
 
 export default Loading;
